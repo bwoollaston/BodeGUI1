c939da8d5b02dbafccfcd5d1478b6c2f130137a3
bwoollaston <bwoollaston@pneumarespiratory.com>
2023-04-06 12:47:54 -0400

    clonned changes

--- BodeGUI1/obj/Debug/net6.0-windows/App.g.i.cs
+++ BodeGUI1/obj/Debug/net6.0-windows/App.g.i.cs
@@ -63,7 +63,7 @@ namespace BodeGUI1 {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/BodeGUI1;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BodeGUI1;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- BodeGUI1/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ BodeGUI1/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -56,7 +56,7 @@ namespace BodeGUI1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/BodeGUI1;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BodeGUI1;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
